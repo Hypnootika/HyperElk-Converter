d22cb3d2984c31170b38852e288b65c23b1f1d67
sensinsane <34210817+sensinsane@users.noreply.github.com>
2023-04-08 17:48:10 +0200

    push
    Signed-off-by: sensinsane <34210817+sensinsane@users.noreply.github.com>

--- HyperElk-Converter Source/Main.py
+++ HyperElk-Converter Source/Main.py
@@ -1,13 +1,19 @@
-from PyQt5 import QtWidgets
+import PyQt5
+from PyQt5 import QtWidgets, QtCore
+
 from HyperElk_idDump_ConverterUI import UiMainWindow
+from idDumpOrganizer import UiDumpOrganizer
 
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
     main_window = QtWidgets.QMainWindow()
+    dump_window = QtWidgets.QWidget()
     ui = UiMainWindow()
     ui.setupUi(main_window)
+    uiD = UiDumpOrganizer()
+    uiD.setupUi(dump_window)
 
     main_window.show()
     sys.exit(app.exec_())
